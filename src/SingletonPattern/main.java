fc4a421e5a97c69333b16023658cc636f9d3a774
Dongmin Kim <kimdongmin68@naver.com>
2023-02-23 03:14:13 +0900

    생성 패턴

--- src/SingletonPattern/main.java
+++ src/SingletonPattern/main.java
@@ -1,7 +1,8 @@
 package SingletonPattern;
 
 /**
- * 클래스의 인스턴스가 하나임을 보장하고 접근할 수 있는 전역적인 접근점을 제공하는 패턴 */
+ * 클래스의 인스턴스가 하나임을 보장하고 접근할 수 있는 전역적인 접근점을 제공하는 패턴
+ * */
 public class main {
     public static void main(String[] args) {
 
